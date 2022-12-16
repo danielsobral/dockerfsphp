a5e38ef6e58fc9dfcedfafea3ee170348ec31b3b
Daniel Sobral <danielsobralnascimento@hotmail.com>
2022-12-15 23:49:37 -0300

    Laravel ainda não esta funcionando, precisa arrumar

--- docker-build.sh
+++ docker-build.sh
@@ -36,21 +36,26 @@ fi
 
 cd $diretorio_raiz/$pasta_projeto;
 
-if [ ! -f $diretorio_raiz/$pasta_projeto/Dockerfile && ! -f $diretorio_raiz/$pasta_projeto/docker-compose.yml ];
+if [ ! -f $diretorio_raiz/$pasta_projeto/Dockerfile -a ! -f $diretorio_raiz/$pasta_projeto/docker-compose.yml ];
 then
         echo "Realizando o download dos arquivos DockerFile e docker-compose.yml";
 	git clone https://github.com/danielsobral/dockerfsphp.git && \
 	echo "Download concluído!"
+	mv $diretorio_raiz/$pasta_projeto/dockerfsphp/* $diretorio_raiz/$pasta_projeto/ && \
+	rm -rf $diretorio_raiz/$pasta_projeto/dockerfsphp
 fi
 
 mkdir $diretorio_raiz/$pasta_projeto/db_data;
 rm $diretorio_raiz/$pasta_projeto/docker-build.sh
 
-echo "==============================================="
-echo "==== Iniciando a construção dos containers ===="
-echo "==============================================="
+echo "==================================================="
+echo "==== Iniciando a construção dos containers PHP ===="
+echo "==================================================="
 sleep 5
 docker build . -t danielsobralnascimento/webserver:1.0 && \
 docker compose up -d && \
 machine_apache=$(docker ps -a | grep '/bin/sh -c' | cut -d " " -f1) && \
-docker exec $machine_apache composer create-project --prefer-dist laravel/laravel blog
+docker exec $machine_apache composer create-project --prefer-dist laravel/laravel blog && \
+echo "====================================================================="
+echo "==== Construção dos containers finalizada #Bora Codar em PHP! :) ===="
+echo "====================================================================="
